4d95eb2f6c587e45773343c1aedbb9eb4967696a
sumzzgit <131842007+sumzzgit@users.noreply.github.com>
2024-06-05 21:46:30 +0530

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+if [[ $run_sonar == "YES" ]]; then
+  echo "Running SonarQube analysis..."
+  mvn clean verify sonar:sonar \
+    -Dsonar.projectKey=test \
+    -Dsonar.projectName="test" \
+    -Dsonar.host.url=http://13.201.65.96:9000 \
+    -Dsonar.token=sqp_86bc3368f6da1889773c3c1ba6f39189ff884436
+else
+  mvn clean verify
+fi
